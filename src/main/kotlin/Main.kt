b3bd0184887c93536fe6f54ee4a7596671d37410
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-02-08 06:10:48 +0300

    Kotlin_020

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var name = "Lauranyaaga"
+    println(name)
+    var age = 13
+    println(age)
+    var phoneNumber = "+254756890234"
+    println(phoneNumber)
+    var weightInKg = "33.23"
+    println(weightInKg)
+    var kenyanCitizen = "true"
+    println(true)
+}
